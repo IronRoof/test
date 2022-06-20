2ab956b584440ec1f6cd3f5bec87a82b82e03f74
IronRoof <107854696+IronRoof@users.noreply.github.com>
2022-06-20 13:01:16 +0100

    Create newtest.js

---
+++ newtest.js
@@ -0,0 +1,15 @@
+function BookWork(girl){
+    var boy = " "
+
+
+    var newClass = {
+        name: "scofield",
+        school: "ths",
+        church: "sda",
+        year: 39
+    }
+    boy = newClass[girl]
+    return boy
+}
+
+console.log(BookWork("name"))
